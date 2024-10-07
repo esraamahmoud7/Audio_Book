6a537fa3a97a204cbdf43d6989309e9176f98e5a
esraa mahmoud <esraaamahmoud77@gmail.com>
2024-10-07 21:41:28 +0300

    finish database

--- lib/views/Register.dart
+++ lib/views/Register.dart
@@ -2,53 +2,106 @@ import 'package:audio_book/Constants.dart';
 import 'package:audio_book/views/logIn.dart';
 import 'package:audio_book/widgets/CustomButton.dart';
 import 'package:audio_book/widgets/TextField.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import '../helper/showSnakeBar.dart';
+import 'Welcome.dart';
 
-import 'Verification.dart';
+// import 'Verification.dart';
 
-class Register extends StatelessWidget {
+class Register extends StatefulWidget {
   const Register({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(left: 40, right: 40),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset("assets/logo.png"),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text("Register" , style: TextStyle(fontSize: 18),),
-            ),
-            SizedBox(height: 15,),
-            CustomTextField(hint: "Email",inVisible: false,onChanged: (data){},),
-            CustomTextField(hint: "Password",inVisible: true,onChanged: (data){},),
-            CustomTextField(hint: "Date of Birth",inVisible: false,onChanged: (data){},),
-            SizedBox(height: 15,),
-            Row(
-              children: [
-                Text("By signing up, you agree to our ",style: TextStyle(fontSize: 16),),
-                Text("Terms ",style: TextStyle(color: KForgetColor,fontSize: 16,fontWeight: FontWeight.bold)),
-                Text(",",style: TextStyle(fontSize: 16)),
-              ],
-            ),
-            Row(
-              children: [
-                Text("Data Policy",style: TextStyle(color: KForgetColor,fontSize: 16,fontWeight: FontWeight.bold)),
-                Text(" and",style: TextStyle(fontSize: 16),),
-                Text(" Cookies Policy",style: TextStyle(color: KForgetColor,fontSize: 16,fontWeight: FontWeight.bold))
-              ],
-            ),
-            SizedBox(height: 20,),
-            CustomButton(colour: KColor, title: "Register", TextColor: Color(0xffffffff), onTap: (){Navigator.push(context, MaterialPageRoute(builder: (context) => Verification()));}, width: 370, height: 65),
-            SizedBox(height: 15,),
-            CustomButton(colour: Color(0xffffffff), title: "Cancel", TextColor: KColor, onTap: (){Navigator.push(context, MaterialPageRoute(builder: (context) => LogIn()));}, width: 370, height: 65)
-          ],
-        ),
-      ),
+  State<Register> createState() => _RegisterState();
+}
 
+class _RegisterState extends State<Register> {
+  String? email,password,date;
+
+  GlobalKey<FormState> formKey=GlobalKey();
+
+  bool isLoading=false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ModalProgressHUD(
+      inAsyncCall: isLoading,
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.only(left: 40, right: 40),
+          child: Form(
+            key: formKey,
+            child: ListView(
+              children: [
+                Image.asset("assets/logo.png",height: 280,),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child:  Text("Register" , style: TextStyle(fontSize: 18),),
+                ),
+                const SizedBox(height: 15,),
+                CustomTextField(hint: "Email",inVisible: false,onChanged: (data){ email = data; },),
+                CustomTextField(hint: "Password",inVisible: true,onChanged: (data){password = data ;},),
+                CustomTextField(hint: "Date of Birth",inVisible: false,onChanged: (data){ date = data ;},),
+                const SizedBox(height: 15,),
+                 Row(
+                  children: [
+                    const Text("By signing up, you agree to our ",style: TextStyle(fontSize: 16),),
+                    Text("Terms ",style: TextStyle(color: KForgetColor,fontSize: 16,fontWeight: FontWeight.bold)),
+                    const Text(",",style: TextStyle(fontSize: 16)),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text("Data Policy",style: TextStyle(color: KForgetColor,fontSize: 16,fontWeight: FontWeight.bold)),
+                    const Text(" and",style: TextStyle(fontSize: 16),),
+                    Text(" Cookies Policy",style: TextStyle(color: KForgetColor,fontSize: 16,fontWeight: FontWeight.bold))
+                  ],
+                ),
+                const SizedBox(height: 20,),
+                CustomButton(colour: KColor, title: "Register", TextColor:const Color(0xffffffff), onTap: ()async
+                {
+                  if (formKey.currentState!.validate()) {
+                    isLoading = true;
+                    setState(() {});
+                    try {
+                        await RegisterUser();
+                        showSnakeBar(
+                        context, 'The account created successfully.');
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => Welcome()));
+                        } on FirebaseAuthException catch (e) {
+                        if (e.code == 'weak-password') {
+                        showSnakeBar(context,
+                        'The password provided is too weak.');
+                        } else if (e.code == 'email-already-in-use') {
+                        showSnakeBar(context,
+                        'The account already exists for that email.');
+                      }
+                    } catch (e) {
+                      print(e);
+                      showSnakeBar(context, 'There was an error.');
+                    }
+                    isLoading = true;
+                    setState(() {});
+                  }
+      
+                }, width: 370, height: 65),
+                const SizedBox(height: 15,),
+                CustomButton(colour:const Color(0xffffffff), title: "Cancel", TextColor: KColor, onTap: (){Navigator.push(context, MaterialPageRoute(builder: (context) => const LogIn()));}, width: 370, height: 65)
+              ],
+            ),
+          ),
+        ),
+      
+      ),
+    );
+  }
+
+  Future<void> RegisterUser() async {
+    UserCredential user = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email!,
+        password: password!,
     );
   }
 }
--- lib/views/logIn.dart
+++ lib/views/logIn.dart
@@ -1,68 +1,119 @@
 import 'package:audio_book/widgets/CustomButton.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 import '../Constants.dart';
+import '../helper/showSnakeBar.dart';
 import '../widgets/TextField.dart';
 import 'Register.dart';
+import 'Welcome.dart';
 
-class LogIn extends StatelessWidget {
+class LogIn extends StatefulWidget {
   const LogIn({super.key});
 
   @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  String? Email,Password;
+  bool isLoading = false;
+
+  GlobalKey<FormState> formKey=GlobalKey();
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(left: 40, right: 40),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset("assets/logo.png"),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 20.0,bottom: 10),
-                child: Text("Login to Your Account  ",style: TextStyle(fontSize: 18),),
-              ),
-            ),
-            CustomTextField(hint : "Email",inVisible: false,onChanged: (data){},),
-            SizedBox(height: 10,),
-            CustomTextField(hint : "Password",inVisible: true,onChanged: (data){},),
-            SizedBox(height: 70,),
-            CustomButton(colour: KColor, TextColor: Color(0xffffffff),title: "Login",onTap: (){},width: 370,height: 65,),
-            SizedBox(height: 5,),
-            Align(
-              alignment:Alignment.centerRight,
-              child: Text("Forget Password ? " ,style: TextStyle(color : KForgetColor,fontWeight: FontWeight.bold,fontSize: 18)),
-            ),
-            SizedBox(height: 15,),
-            Align(
-              alignment: Alignment.center,
-              child: Text("Or Login With ?",style: TextStyle(fontSize: 20),),
-            ),
-            SizedBox(height: 10,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+    return ModalProgressHUD(
+      inAsyncCall: isLoading,
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.only(left: 40, right: 40),
+          child: Form(
+            key: formKey,
+            child: ListView(
               children: [
-                CustomContainer(image: "assets/logos_google.png"),
-                CustomContainer(image: "assets/logos_facebook.png"),
-                CustomContainer(image: "assets/logos_twitter.png"),
+                Image.asset("assets/logo.png",height: 280,),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 20.0,bottom: 10),
+                    child: Text("Login to Your Account  ",style: TextStyle(fontSize: 18),),
+                  ),
+                ),
+                CustomTextField(hint : "Email",inVisible: false,onChanged: (data){ Email = data;},),
+                SizedBox(height: 10,),
+                CustomTextField(hint : "Password",inVisible: true,onChanged: (data){ Password = data;},),
+                SizedBox(height: 70,),
+                CustomButton(colour: KColor, TextColor: Color(0xffffffff),title: "Login",width: 370,height: 65,onTap: ()async
+                {
+                  if (formKey.currentState!.validate()) {
+                    isLoading = true;
+                    setState(() {});
+                    try {
+                      await LoginUser();
+                      showSnakeBar(
+                          context, "we miss you :').");
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => Welcome()));
+                    } on FirebaseAuthException catch (e) {
+                      if (e.code == 'user-not-found') {
+                        showSnakeBar(context,
+                            'No user found for that email.');
+                      } else if (e.code == 'wrong-password') {
+                        showSnakeBar(context,
+                            'Wrong password provided for that user.');
+                      }
+                    }
+                    catch (e) {
+                      print(e);
+                      showSnakeBar(context, 'There was an error.');
+                    }
+                    isLoading = false;
+                    setState(() {});
+                  }
+                }),
+                SizedBox(height: 5,),
+                Align(
+                  alignment:Alignment.centerRight,
+                  child: Text("Forget Password ? " ,style: TextStyle(color : KForgetColor,fontWeight: FontWeight.bold,fontSize: 18)),
+                ),
+                SizedBox(height: 15,),
+                Align(
+                  alignment: Alignment.center,
+                  child: Text("Or Login With ?",style: TextStyle(fontSize: 20),),
+                ),
+                SizedBox(height: 10,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CustomContainer(image: "assets/logos_google.png"),
+                    CustomContainer(image: "assets/logos_facebook.png"),
+                    CustomContainer(image: "assets/logos_twitter.png"),
+                  ],
+                ),
+                SizedBox(height: 15,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Don't have an account ?",style: TextStyle(fontSize: 16),),
+                    GestureDetector(
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => Register()));
+                    },
+                    child: Text(" Register",style: TextStyle(color: KForgetColor,fontSize:18 ,fontWeight: FontWeight.bold),))
+                  ],
+                )
               ],
             ),
-            SizedBox(height: 15,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Don't have an account ?",style: TextStyle(fontSize: 16),),
-                GestureDetector(
-                onTap: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => Register()));
-                },
-                child: Text(" Register",style: TextStyle(color: KForgetColor,fontSize:18 ,fontWeight: FontWeight.bold),))
-              ],
-            )
-          ],
+          ),
         ),
       ),
+    );
+  }
+
+  Future<void> LoginUser() async {
+    UserCredential userCredental = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: Email!,
+        password: Password!
     );
   }
 }
